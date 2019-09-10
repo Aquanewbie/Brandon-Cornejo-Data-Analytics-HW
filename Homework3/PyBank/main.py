1c408c0b8923d281c4cdbde53fe2facd64c0d7a6
Brandon Cornejo <BE_cornejo8@yahoo.com>
2019-09-10 13:12:33 -0700

    Done with pyBank

--- Homework3/PyBank/main.py
+++ Homework3/PyBank/main.py
@@ -14,7 +14,7 @@ with open(csvpath) as rowcount:
     for i in rowcount:
         num_lines += 1
 months = num_lines
-print (months)
+print (F'Total Months: {months}')
 
 'Add sum of cells in Profit/Loss'
 nettotal = 0
@@ -24,7 +24,9 @@ with open(csvpath, newline="") as rowcount:
     for a in rowcountreader:
         'print(a)'
         nettotal += int(a[1])
-print (nettotal)
+
+nettotal = '${:.2f}'.format(nettotal)
+print (F'Net Total: {nettotal}')
 
 'The average of the changes in "Profit/Losses" over the entire period'
 
@@ -39,15 +41,37 @@ with open(csvpath, newline="") as rowcount:
             changestotal += (int(b[1]) - prevline)
             prevline = int(b[1])
 
+AvChange = '${:.2f}'.format(int(changestotal)/totalchanges)
+changestotal = '${:.2f}'.format(changestotal)
 print (F'Changes total: {changestotal}')
-pqt = int(changestotal)/totalchanges
-print (pqt)
+print (F'Average Change: {AvChange}')
+
 
 with open(csvpath, newline="") as rowcount:
-    changestotal = 0
-    totalchanges = int(months-1)
+    previousprofit = 867884
+    greatestincrease= 0
+    greatestdecrease= 0
     rowcountreader = csv.reader(rowcount, delimiter=",")
     next(rowcountreader)
+    for c in rowcountreader: 
+        if int(c[1]) > previousprofit:
+            if int(c[1]) > greatestincrease:
+                greatestincrease = (int(c[1]) - previousprofit)
+            previousprofit = int(c[1])
+        else:
+            if int(c[1]) < greatestdecrease:
+                greatestdecrease= (int(c[1]) - previousprofit)
+            previousprofit = int(c[1])
+
+        
+
+greatestincrease = '${:.2f}'.format(int(greatestincrease))
+print (F'Greatest Increase: {greatestincrease}')
+greatestdecrease = '${:.2f}'.format(int(greatestdecrease))
+print (F'Greatest Decrease: {greatestdecrease}')
+                
+
+
 
 
 'Total Months: 86'
