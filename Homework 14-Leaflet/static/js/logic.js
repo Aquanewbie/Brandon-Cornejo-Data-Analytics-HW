6a9b1df40b4120e76b5ec90b9f71b711b12dd445
Brandon Cornejo <BE_cornejo8@yahoo.com>
2020-01-19 22:12:56 -0800

    Finihed Leaflet

--- Homework 14-Leaflet/static/js/logic.js
+++ Homework 14-Leaflet/static/js/logic.js
@@ -11,39 +11,49 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(EarthquakeMap);
 
-
+var colorrange = d3.scaleLinear().domain([4.5,6.5]).range(['#FFFF00', '#ff0000'])
+var MarkerRadius = d3.scaleLinear().domain([4.5,6.5]).range([5, 20])
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson", function(data) {
-    console.log(data[0]);
-    
+
+    // Run through a Magnitude of Colors
+    function magnitudecolors (value) {
+        return (colorrange(value))};
+
     L.geoJson(data, {
         style: function(feature) {
-            return {
-                color: "green"
+            return { color: magnitudecolors(feature.properties.mag)
             };
         },
         pointToLayer: function(feature, latlng) {
             return new L.CircleMarker(latlng, {
-                radius: 10, 
-                fillOpacity: 0.85
+                radius: MarkerRadius(feature.properties.mag), 
+                fillOpacity: 1
             });
         },
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(feature.properties.NAME);
+            layer.bindPopup("Title: " + feature.properties.title + "<br>" + "Magnitude: " + feature.properties.mag );
         }
     }).addTo(EarthquakeMap)});
 
-// var greenIcon = L.icon({
-//     iconUrl: 'leaf-green.png',
-//     shadowUrl: 'leaf-shadow.png',
-    
-//     iconSize:     [38, 95], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-//     });
 
-// // Typical Magnitudes ranges -1,0,1,2,3,4,5,6,7,8,9,10
-// var color = d3.scale.linear().domain([Magmin,Magmax])
-//         .range(['#FFFF00', '#FF0000']);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    
+var div = L.DomUtil.create('div', 'info legend'),
+    grades = [4.5, 4.75, 5.0, 5.25, 5.5, 5.75, 6.0],
+    labels = [];
+var LegendColorRange = d3.scaleLinear().domain([4.5,6.5]).range(['#FFFF00', '#ff0000'])
+// loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + LegendColorRange(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+    
+    return div;
+    };
+
+legend.addTo(EarthquakeMap)
